4a9340f7de70f091739c0a5a17461ab752731c03
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-07-17 14:57:02 -0300

    Little script to search an object in the full DB
    Searchs for an object in every table and view in the whole DB.

---
+++ workingScripts/objectFullDBSearch.sql
@@ -0,0 +1,16 @@
+/* Search for objects, change the value in @mySearchObject */
+
+------------------------------------------------------------------------------------------------------------------------------
+-- Set the variable to search for the object
+DECLARE @mySearchObject VARCHAR(250)
+SET @mySearchObject = 'idComprobante'
+
+------------------------------------------------------------------------------------------------------------------------------
+-- Script logic
+SELECT COLUMN_NAME AS 'columnName',
+       TABLE_NAME AS  'tableName'
+	FROM INFORMATION_SCHEMA.COLUMNS
+		WHERE COLUMN_NAME LIKE '%'+@mySearchObject+'%'
+			ORDER BY TableName,
+					 ColumnName;
+
